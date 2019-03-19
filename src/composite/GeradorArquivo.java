eb96f5f440e0d51c81fcabc7e1444ff5b04b0a54
valdomirocm2016 <valdomirocm@gmail.com>
2019-03-19 15:25:50 -0300

    Criação das Classes do Padrão de Projeto Composite

---
+++ src/composite/GeradorArquivo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composite;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ *
+ * @author Valdomiro
+ */
+public abstract class GeradorArquivo {
+    
+    private Processador processador;
+    
+    public GeradorArquivo(Processador processador){
+        
+        this.processador=processador;
+        
+    }
+    
+    public final void gerarArquivo(String nome, Map<String,Object> propriedades) throws IOException{
+        //byte[] bytes=null;
+       /* if(tipo.equals("PROPRIEDADES_CRIPTOGRAFADO")){
+            ProcessaPropriedadesCriptografado p = new ProcessaPropriedadesCriptografado();
+            
+            
+            bytes= p.processaPropriedadesCriptografado(propriedades);
+        }else if(tipo.equals("XML_COMPACTADO")){
+            ProcessaXmlCompactado pp= new ProcessaXmlCompactado();
+            
+            bytes=pp.processaXMLCompactado(propriedades);
+            
+        }else{
+            System.out.println("Desconheço essa opção");
+            return;
+        }*/
+       String conteudo= gerarConteudo(propriedades);
+       byte[] bytes = processador.processaConteudo(conteudo.getBytes());
+       
+        FileOutputStream fileout= new FileOutputStream(nome);
+        fileout.write(bytes);
+        fileout.close();
+    }
+    /*public String gerarConteudo(Map<String,Object> propriedades){
+        return "";
+    }*/
+    
+    protected abstract String gerarConteudo(Map<String,Object> propriedades);
+    
+    
+    
+    
+   
+   
+    
+    
+    
+}
---
+++ src/composite/GeradorArquivoXml.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composite;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ *
+ * @author Valdomiro
+ */
+public class GeradorArquivoXml extends GeradorArquivo {
+    
+    /*public byte[] processaXMLCompactado(Map<String,Object> propriedades) throws IOException{
+        //GeraXML
+        String conteudo= geraConteudoXML(propriedades);
+        return processaCompactacao(conteudo.getBytes());     
+    }*/
+    public GeradorArquivoXml(Processador processador){
+        super(processador);
+    }
+    @Override
+    protected String gerarConteudo(Map<String,Object> propriedades){
+        StringBuilder propFileBuilder = new StringBuilder();
+            propFileBuilder.append("<properties>");
+            for(String prop: propriedades.keySet()){
+                propFileBuilder.append("<"+prop+">"+propriedades.get(prop)+"</"+prop+">");
+            }
+            propFileBuilder.append("</propriedades>");
+            return propFileBuilder.toString();
+    }
+    
+}
